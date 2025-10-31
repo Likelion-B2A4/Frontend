5e0ef52382f40b79d372029524f3327e7e55b6ed
이윤서 <yyoo04@naver.com>
2025-11-01 00:06:09 +0900

    #1 [UI] 스플래시 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import SplashPageWeb from './pages/splash.tsx';
+import Intro from './pages/Intro.tsx';
+import LogIn from './pages/LogIn.tsx';
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<SplashPageWeb />} />
+        <Route path="/logointro" element={<Intro />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/LogIn.tsx
@@ -0,0 +1,53 @@
+import { use, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '../components/Button';
+
+const LogIn = () => {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const nav = useNavigate();
+
+  const isFormInvalid = id === '' || password === '';
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log('아이디:', id);
+    console.log('비밀번호:', password);
+
+    nav('/main');
+  };
+
+  return (
+    <div className="flex h-screen flex-col justify-center items-center p-[16px]">
+      <div className="mb-[60px]">
+        <img src={'/sonbit.svg'} alt="손빛 로고" className="w-[100px] h-[100px]" />
+        <div className="text-alice text-2xl text-transparent flex justify-center bg-clip-text bg-gradient-to-br from-[#0F58FF] to-[#3FB6FF]">
+          손빛
+        </div>
+      </div>
+      <div className="gap-y-[16px] flex flex-col py-0">
+        <input
+          type="text"
+          placeholder="아이디를 입력하세요"
+          onChange={(e) => setId(e.target.value)}
+          value={id}
+          className="flex text-[16px] w-[320px] h-[48px] pl-[8px] pr-[16px] items-center border-x-transparent border-t-transparent border-b-1 border-gray-400 outline-0  focus:border-b-[#0F58FF] caret-[#0F58FF]"
+        />
+        <input
+          type="text"
+          placeholder="비밀번호를 입력하세요"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="flex text-[16px] w-[320px] h-[48px] pl-[8px] pr-[16px] items-center border-x-transparent border-t-transparent border-b-1 border-gray-400 outline-0  focus:border-b-[#0F58FF] caret-[#0F58FF]"
+        />
+      </div>
+      <div className="gap-[10px] flex flex-col mt-[40px] py-[12px]">
+        <Button children="회원가입" />
+        <Button disabled={isFormInvalid} variant="colored" children="로그인" />
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
