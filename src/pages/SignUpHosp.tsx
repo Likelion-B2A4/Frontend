8963058d24e0abb0965b885aa020b035ea1239ec
이윤서 <yyoo04@naver.com>
2025-11-04 13:45:10 +0900

    #13 [MOD] Step1Form 컴포넌트 분리

--- src/pages/SignUpHosp.tsx
+++ src/pages/SignUpHosp.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import FormInput from '../components/FormInput';
 import WeeklyButton from '../components/WeeklyButton';
 import Step2Form from '../components/Step2Form';
+import Step1Form from '../components/Step1Form';
 
 interface IOperatingTime {
   mon: string | null;
@@ -24,73 +25,12 @@ interface IFormData {
   mainImage: File | null;
 }
 
-interface Step1FormProps {
-  formData: IFormData;
-  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onKeyDown: (e: React.KeyboardEvent<HTMLInputElement | HTMLSelectElement>) => void;
-}
-
 interface FileFormProps {
   mainImage: File | null;
   handleFileChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 // 폼 컴포넌트
-
-const Step1Form = ({ formData, handleInputChange, onKeyDown }: Step1FormProps) => {
-  return (
-    <div id="step1" className="flex flex-col gap-y-[24px] min-h-[290px]">
-      {/* 병원가입 폼-1 */}
-      <div className="flex flex-row justify-between">
-        <div>
-          병원명
-          <FormInput
-            label="form"
-            placeholder="병원명을 입력하세요"
-            name="hospitalName"
-            value={formData.hospitalName}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          {/* 드롭다운 선택지 */}
-          진료 과목
-          <FormInput
-            label="form"
-            placeholder="진료 과목을 입력하세요"
-            name="subject"
-            value={formData.subject}
-            onChange={handleInputChange}
-          />
-        </div>
-      </div>
-      <div>
-        주소
-        <FormInput
-          label="form"
-          placeholder="병원 주소를 입력하세요"
-          name="address"
-          value={formData.address}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div>
-        연락처
-        <FormInput
-          label="form"
-          placeholder="병원 연락처를 입력하세요"
-          hint="숫자만 입력해주세요"
-          name="contactNumber"
-          value={formData.contactNumber}
-          onChange={handleInputChange}
-          onKeyDown={onKeyDown}
-        />
-      </div>{' '}
-      {/* 숫자만 입력하면 하이픈 형태로 저장 */}
-      {/* 다 입력하면 자동으로 페이지 넘어가게 */}
-    </div>
-  );
-};
 
 const FileForm = ({ mainImage, handleFileChange }: FileFormProps) => {
   const [previewImage, setPreviewImage] = useState<string | null>(null);
