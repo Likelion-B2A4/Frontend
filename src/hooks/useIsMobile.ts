89cf4f713941469c13a785884416ffce339ec735
이윤서 <yyoo04@naver.com>
2025-11-05 23:06:01 +0900

    #8 [ADD] 모바일 훅 추가

---
+++ src/hooks/useIsMobile.ts
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+
+export const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    const mobileRegex = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+    setIsMobile(mobileRegex.test(userAgent));
+  }, []);
+};
