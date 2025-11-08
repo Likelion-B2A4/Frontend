82274da3949e0655b8a4d0eec84d3051d0303b6c
이윤서 <yyoo04@naver.com>
2025-11-08 12:30:51 +0900

    #22 [FEAT] ToggleSwitch 컴포넌트 구현

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,11 +1,27 @@
 import Topbar from '../layouts/Topbar';
+import { topHeader } from '../styles/typography';
+import ToggleSwitch from '../components/ToggleSwitch';
+import { useState, type ReactEventHandler } from 'react';
 
 const Setting = () => {
+  const [isOn, setIsOn] = useState(true);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsOn(e.target.checked);
+  };
+
   return (
     <div>
       <Topbar title="설정" type="header" />
-
-      <div>설정 페이지입니다</div>
+      {/* body */}
+      <div style={topHeader}>
+        <div className="flex flex-row justify-between py-[16px] px-[20px]">
+          <div className="">위치 정보 권한</div>
+          <ToggleSwitch id="location" checked={isOn} onChange={handleChange} />
+        </div>
+        <div className="py-[16px] px-[20px]">서비스 안내</div>
+        <div className="py-[16px] px-[20px]">로그아웃</div>
+      </div>
     </div>
   );
 };
