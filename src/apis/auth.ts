193a2b510b4599c8085ae4c2b0359d0a540e2b0d
이윤서 <yyoo04@naver.com>
2025-11-20 23:10:56 +0900

    #79 [FEAT] 병원 로그인 구현

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,27 +1,40 @@
 import instance from '../utils/axiosInstance';
 import { useAuthStore } from '../hooks/useAuthStore';
 
-interface LoginPatientPayload {
+interface LoginPayload {
   loginId: string;
   pwd: string;
 }
 
-interface LoginPatientResponse {
+interface LoginResponse {
   isSuccess: boolean;
   message: string;
   patientId: string;
   name: string;
   accessToken: string;
   refreshToken: string;
+  imageUrl?: string;
 }
 
-export const loginPatientApi = async (payload: LoginPatientPayload) => {
-  const response = await instance.post<LoginPatientResponse>('/api/patients/login', payload);
+export const loginPatientApi = async (payload: LoginPayload) => {
+  const response = await instance.post<LoginResponse>('/api/patients/login', payload);
   return response.data;
 };
 
 export const logoutPatientApi = async () => {
   await instance.post('/api/patients/login');
+  useAuthStore.getState().clearAuth();
+  localStorage.removeItem('accessToken');
+  localStorage.removeItem('refreshToken');
+};
+
+export const loginHospitalApi = async (payload: LoginPayload) => {
+  const response = await instance.post<LoginResponse>('/api/hospitals/login', payload);
+  return response.data;
+};
+
+export const logoutHospitalApi = async () => {
+  await instance.post('/api/hospitals/login');
   useAuthStore.getState().clearAuth();
   localStorage.removeItem('accessToken');
   localStorage.removeItem('refreshToken');
--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -4,7 +4,7 @@ import Button from '../components/Button';
 import { isValidPassword } from '../utils/validation';
 import FormInput from '../components/FormInput';
 import { useIsMobile } from '../hooks/useIsMobile';
-import { loginPatientApi } from '../apis/auth';
+import { loginHospitalApi, loginPatientApi } from '../apis/auth';
 import { useAuthStore } from '../hooks/useAuthStore';
 
 type LoginFormInputs = {
@@ -37,54 +37,35 @@ const LogIn = () => {
   };
 
   const onSubmit: SubmitHandler<LoginFormInputs> = async (data) => {
-    // e.preventDefault()는 rhfHandleSubmit이 자동으로 해줍니다.
-
-    // console.log('아이디:', data.id); // 'id' 대신 'data.id'
-    // console.log('비밀번호:', data.password); // 'password' 대신 'data.password'
-
-    // if (data.id === mockData.id && data.password === mockData.password) {
-    //   const payload = {
-    //     id: data.id,
-    //     password: data.password,
-    //     deviceType: isMobile ? 'mobile' : 'desktop',
-    //   };
-
-    //   console.log('전송할 데이터:', payload); // try { ... API 호출 ... }
-    // } else {
-    //   // window.alert('로그인 실패!');
-    //   // setError('id', {
-    //   //   type: 'unauthorized',
-    //   //   message: '아이디 또는 비밀번호가 일치하지 않습니다.',
-    //   // });
-    //   setError('password', {
-    //     type: 'unauthorized',
-    //   });
-    // }
-
-    // try {
-    //   // API 엔드포인트는 하나만 있어도 됩니다.
-    //   await axios.post('https://api.example.com/auth/login', payload);
-    //   alert('로그인 성공!');
-
-    //   // 백엔드 로그 확인: { email: '...', password: '...', deviceType: 'mobile' }
-    //   console.log('전송한 데이터:', payload);
-
-    // } catch (error) {
-    //   alert('로그인 실패');
-    // }
     try {
-      const response = await loginPatientApi({
-        loginId: data.id,
-        pwd: data.password,
-      });
-      console.log('로그인 성공');
+      let response;
+      if (isMobile) {
+        response = await loginPatientApi({
+          loginId: data.id,
+          pwd: data.password,
+        });
+        console.log('로그인 성공');
 
-      const { accessToken, refreshToken } = response;
-      setTokens(accessToken, refreshToken);
-      localStorage.setItem('accessToken', accessToken);
-      localStorage.setItem('refreshToken', refreshToken);
+        const { accessToken, refreshToken } = response;
+        setTokens(accessToken, refreshToken);
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
 
-      nav('/hospitalmap');
+        nav('/hospitalmap');
+      } else {
+        response = await loginHospitalApi({
+          loginId: data.id,
+          pwd: data.password,
+        });
+        console.log('로그인 성공');
+
+        const { accessToken, refreshToken } = response;
+        setTokens(accessToken, refreshToken);
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+
+        nav('/select-doctor');
+      }
     } catch (error: any) {
       console.error('로그인 실패: ', error);
 
